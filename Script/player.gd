3f5f917540e92d120ed4320f5e7814d54130a0d2
Jelombo <95728999+Jelombo@users.noreply.github.com>
2025-03-19 13:25:33 -0500

    Add files via upload

--- Script/player.gd
+++ Script/player.gd
@@ -3,8 +3,13 @@ extends CharacterBody2D
 class_name Player
 
 @onready var animated_sprite_2d: AnimationController = $playerAnimations
+@onready var timerDash: Timer = $dashTimer
 
-const SPEED = 5000.0
+var SPEED = 5000.0
+
+
+func _end_dash():
+	SPEED = 5000.0
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -20,5 +25,9 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite_2d.play_idle_animation()
 		
-
+	if Input.is_action_just_pressed("Dash"):
+		timerDash.start()
+		SPEED = 10000.0
+		get_tree().create_timer(0.5).timeout.connect(_end_dash)
+	
 	move_and_slide()
